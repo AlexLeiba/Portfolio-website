2e34be10f3f38229e73b868e0aac92461ef22cce
Leiba Alex <alexandru@enovatika.com>
2025-04-21 12:59:24 +0300

    floating icons

--- components/components-pages/Hero.tsx
+++ components/components-pages/Hero.tsx
@@ -7,7 +7,6 @@ import { Spacer } from '../UI/spacer/spacer';
 import { FileDown } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { useTheme } from '@/lib/themeContext';
-import { floatingIcons } from '@/consts/localDB';
 
 function Hero() {
   const { darkMode } = useTheme();
@@ -106,3 +105,129 @@ function Hero() {
 }
 
 export default Hero;
+
+export const floatingIcons = [
+  {
+    source: '/rocket.svg',
+    width: 20,
+    height: 20,
+    animate: {
+      translateY: 500,
+      transition: { duration: 100 },
+      translateX: -4000,
+      rotate: -20,
+      scale: -1,
+    },
+    className: 'absolute top-[100px] right-[-200px] cursor-move rotate-45',
+    image: {
+      className: 'rotate-[220deg]',
+    },
+  },
+
+  {
+    source: '/categories/React.svg',
+    width: 40,
+    height: 40,
+    animate: {
+      translateX: 250,
+      translateY: 20,
+      rotate: 360,
+      transition: { duration: 15 },
+    },
+    className: 'absolute top-0 left-0 cursor-move',
+    image: {
+      className: '',
+    },
+  },
+  {
+    source: '/categories/NextJS.svg',
+    width: 50,
+    height: 50,
+    animate: {
+      translateY: -500,
+      rotate: 20,
+      transition: { duration: 50 },
+      scale: 1,
+    },
+    className: 'absolute bottom-0 right-0 cursor-move',
+    image: {
+      className: '',
+    },
+  },
+  {
+    source: '/categories/tailwind.svg',
+    width: 30,
+    height: 30,
+    animate: {
+      translateX: 110,
+      rotate: 360,
+      translateY: 100,
+      transition: { duration: 20 },
+    },
+    className: 'absolute top-[20%] -left-[100px] cursor-move',
+    image: {
+      className: '',
+    },
+  },
+  {
+    source: '/categories/TypeScript.svg',
+    width: 30,
+    height: 30,
+    animate: {
+      translateX: -150,
+      rotate: 30,
+      transition: { duration: 20 },
+      scale: 1,
+    },
+    className: 'absolute top-[5%] -right-[100px] sm:-right-[150px] cursor-move',
+    image: {
+      className: '',
+    },
+  },
+  {
+    source: '/categories/JavaScript.svg',
+    width: 40,
+    height: 40,
+    animate: {
+      translateX: 50,
+      translateY: 60,
+      transition: { duration: 20 },
+    },
+    className: 'absolute top-[50%] -left-[50px] cursor-move',
+    image: {
+      className: '',
+    },
+  },
+  {
+    source: '/categories/CSS.svg',
+    width: 40,
+    height: 40,
+    animate: {
+      translateX: 100,
+      rotate: 180,
+      translateY: 100,
+
+      transition: { duration: 20 },
+      scale: -1,
+    },
+    className: 'absolute top-[35%] -left-[50px] cursor-move',
+    image: {
+      className: '',
+    },
+  },
+  {
+    source: '/categories/HTML.svg',
+    width: 60,
+    height: 60,
+    animate: {
+      translateX: 60,
+      rotate: 180,
+      transition: { duration: 30 },
+      scale: -1,
+    },
+    className: 'absolute top-[50%] -left-[50px] cursor-move',
+    image: {
+      className: '',
+    },
+  },
+];
