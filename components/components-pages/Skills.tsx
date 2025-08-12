ac6831b4b9ef6d943c8c4c46c547bfc4d7257177
AlexLeiba <Alexleiba13@gmail.com>
2025-08-12 20:02:52 +0300

    skills responsive

--- components/components-pages/Skills.tsx
+++ components/components-pages/Skills.tsx
@@ -1,10 +1,10 @@
-'use client';
-import React from 'react';
-import { Spacer } from '../UI/spacer/spacer';
-import { Col, Row } from '../UI/Grid';
-import { cn } from '@/lib/utils';
-import { useRouter } from 'next/navigation';
-import { skillsData } from '@/consts/localDB';
+"use client";
+import React from "react";
+import { Spacer } from "../UI/spacer/spacer";
+import { Col, Row } from "../UI/Grid";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { skillsData } from "@/consts/localDB";
 
 function About() {
   const router = useRouter();
@@ -13,11 +13,11 @@ function About() {
     <div>
       <Row>
         <Col lgOffset={2} lg={8}>
-          <div className='flex justify-center '>
-            <h2 className='text-center' data-aos='fade-left'>
+          <div className="flex justify-center ">
+            <h2 className="text-center" data-aos="fade-left">
               Ski
             </h2>
-            <h2 className=' text-primary-400' data-aos='fade-right'>
+            <h2 className=" text-primary-400" data-aos="fade-right">
               lls
             </h2>
           </div>
@@ -28,18 +28,18 @@ function About() {
       <Row>
         {skillsData.map((data, index) => {
           return (
-            <Col key={index} className='w-full mb-6' lg={2} md={2} sm={2}>
+            <Col key={index} className="w-full mb-6" lg={2} md={2}>
               <div
                 data-aos-delay={index * 10}
-                data-aos='fade-up'
+                data-aos="fade-up"
                 onClick={() =>
-                  data.title === 'Projects'
-                    ? router.push('#projects')
+                  data.title === "Projects"
+                    ? router.push("#projects")
                     : () => {}
                 }
                 className={cn(
-                  data.title === 'Projects' && 'cursor-pointer',
-                  'dark:bg-baseline-900 bg-baseline-50 w-full h-24 rounded-xl mb-2 gap-4 shadow-lg'
+                  data.title === "Projects" && "cursor-pointer",
+                  "dark:bg-baseline-900 bg-baseline-50 w-full h-24 rounded-xl mb-2 gap-4 shadow-lg"
                 )}
               >
                 <div
@@ -48,12 +48,12 @@ function About() {
                   }}
                   key={index}
                   className={cn(
-                    ' flex justify-center items-center gap-4 border mb-2 border-primary-200  h-full p-4 rounded-xl flex-wrap overflow-hidden'
+                    " flex justify-center items-center gap-4 border mb-2 border-primary-200  h-full p-4 rounded-xl flex-wrap overflow-hidden"
                   )}
                 >
-                  <div className='flex items-center  gap-2'>
-                    <div className='flex items-center justify-center gap-2'>
-                      <p className='text-black dark:text-white  body-base text-center  font-bold font-mono'>
+                  <div className="flex items-center  gap-2">
+                    <div className="flex items-center justify-center gap-2">
+                      <p className="text-black dark:text-white  body-base text-center  font-bold font-mono">
                         {data.title}
                       </p>
 
